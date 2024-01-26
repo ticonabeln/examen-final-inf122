4281ae01610a4e1f8594136b754083ae1a3753ae
ticonabeln <157810827+ticonabeln@users.noreply.github.com>
2024-01-26 14:46:56 +0000

    Asingnacion de Pokemon

---
+++ src/components/pokemon.js
@@ -0,0 +1,29 @@
+"use client"
+import Image from "next/image";
+import style from "./pokemon.module.css";
+import { useEffect, useState } from "react";
+
+function pokemon (){
+    const[pokemon,setpokemon]=useState("/vercel.svg");
+    const[estado, setEstado]=useState("esperando")
+    const[habilidad, setHabilidad]=useState("habilidad")
+    const url="https://pokeapi.co/api/v2/pokemon-form/175/";
+    useEffect(()=>{
+        fetch(url).then(res=>res.json())
+        .then(data=>{
+            setpokemon(data.sprites.front_default)
+            setEstado(data.species.name)
+            setHabilidad(data.abilities[0].ability.name)
+        }); 
+
+    },[]);
+    return(
+        <div className={style.contenedor}>
+            <h1>{estado}</h1>
+            <Image src={pokemon}alt="pokemon" width={500} height={500}/>
+            <h1>{habilidad}</h1>
+        </div>
+    )
+}
+
+export default pokemon;
